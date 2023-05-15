7a9199da9dc0d7fe7826fc4f6ec04699237e57e7
XamaX <dimas_03@bk.ru>
2023-05-15 21:51:37 +0500

    Refactored client.py

--- client/src/main/python/client.py
+++ client/src/main/python/client.py
@@ -4,13 +4,19 @@ HOST = 'localhost'
 PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(2)
     s.connect((HOST, PORT))
-    message = "https://ura.news/news/1052648334"
+    message = "https://dvsemenov.ru/peredacha-fajla-cherez-soket-v-python-3/"
     s.sendall(message.encode())
     with open("file.html", 'w', encoding='utf-8') as f:
         while True:
-            data = s.recv(1024)
-            if not data:
+            try:
+                data = s.recv(1024)
+                if not data:
+                    raise socket.timeout()
+                f.write(data.decode('utf-8', errors='ignore'))
+            except socket.timeout:
                 s.close()
                 break
-            f.write(data.decode('utf-8', errors='ignore'))
+
+    print("Successful!")
